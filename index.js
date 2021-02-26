8c7813a57ef4001dcdb71b3b3aaf4f5887dca91e
Nikola <Nikola44>
2021-02-26 20:18:09 +0300

    1-st commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// 'use strict';
+let min = Math.floor(Math.random()*10);
+console.log(min);
+
+let number = 4.6; 
+console.log(4/0);
+
+console.log(vasya); //
+var vasya = "ivan";
+
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarries: false
+};
+
+console.log(obj);
+
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', "");
+
+answers[1] = prompt('Как ваше фамилия?', "");
+
+answers[2] = prompt('Сколько вам лет?', "");
+
+console.log(answers);
+
+document.write(answers); 
+console.log("1");

df9f7ce189cb3d911519fa2bbbe8e1053786766e
Nikola <Nikola44>
2021-02-27 17:53:41 +0300

    14

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
-let numberOfFilms = prompt("Сколько фильмов вы уже смотрели?", "");
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+"use strict";
 
-let lastMovie = prompt("Какой последний фильм смотрели?", "");
-let lastRate = prompt("На сколько его оцените?", "");
-let lastRate1 = prompt("На сколько его оцените?", "");
+let r = Math.floor(Math.random()*10);
+if (4 == r) {
+    console.log("OK!");
+} else {
+    console.log("ERRROR");
+}
+console.log(r);
 
+const num = 50;
+if (num < 49) {
+    console.log('ERRRRRKA');
+} else if (num > 100) {
+    console.log("mnogo");
+} else {
+    console.log("OK!");
+}
 
+(num === 50) ? console.log("OK!") : console.log('ERRRRRKA');
 
-personalMovieDB.movies = {
-    [lastMovie]: lastRate
-};
+const numx = 50;
+switch (numx) {
+    case 49:
+        console.log("erka");
+        break;
+    case 100:
+        console.log('ERKA');
+        break;
+    case 50:
+        console.log("GOOD");
+        break;
+    default:
+        console.log("ne v etot raz");
+        break;
+}
 
-console.log(personalMovieDB);
-
+const num1 = 50;
+while (num < 55) {
+    console.log(num);
+    num++
+    
+}
